c6ef52d25b6d6b93158c386dc96adc865f22eda6
Diego Vicuna <diegovicuna@Diegos-MacBook-Pro.local>
2025-09-19 20:16:13 -0500

    feature: update package lock

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: process.env.NODE_ENV === 'production' ? '/leo-invitation/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/javi-invitation/' : '/',
 });
